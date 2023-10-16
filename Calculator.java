5f59c302b01af356571e95a9230802ab675e24b8
MehakNadeem10 <122683714+MehakNadeem10@users.noreply.github.com>
2023-10-16 11:02:27 +0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+package pk.edu.pucit;
+
+public class Calculator {
+public int add(int a, int b) {
+int result = a + b;
+return result;
+}
+public int multiply(int a, int b) {
+int result = a * b;
+return result;
+}
+public int calculate(int x, int y) {
+// Some complex calculations here
+int intermediateResult = add(x,y);
+int finalResult = multiply(intermediateResult, 2);
+return finalResult;
+}
+
+public static void main(String[] args) {
+Calculator calculator = new Calculator();
+int result = calculator.calculate(5, 3);
+System.out.println("Result: " + result);
+}
+}
